6d5c9a31a4c9484dc45522958b8ee85046002654
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2024-10-03 22:31:54 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import models.Entities.Account;
+import models.Exceptions.DomainExceptions;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    try {
+        System.out.println("Enter account data:");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        String holder = sc.next();
+        System.out.print("Initial balance: ");
+        double initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+        Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+        acc.withdraw(amount);
+    }
+    catch (DomainExceptions e) {
+        System.out.println(e.getMessage());
+    }
+
+    }
+}
---
+++ src/models/Exceptions/DomainExceptions.java
@@ -0,0 +1,8 @@
+package models.Exceptions;
+
+public class DomainExceptions  extends Exception{
+
+    public DomainExceptions(String msg) {
+        super(msg);
+    }
+}
